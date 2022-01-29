dd9f056692b58050d8e08cbde7470505c583ca94
heliumbrain <nils@kanevad.se>
2022-01-30 00:09:16 +0100

    Added handler to fetch all users

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use axum::{routing::{get, post}, http::StatusCode, response::IntoResponse, Json, Router, AddExtensionLayer};
-use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-use axum::extract::Extension;
-use skytable::actions::Actions;
-use skytable::ConnectionBuilder;
-use uuid::Uuid;
 
+use axum::{
+	AddExtensionLayer,
+	http::StatusCode,
+	Json,
+	response::IntoResponse, Router, routing::{get, post},
+};
+use axum::extract::Extension;
+use serde::{Deserialize, Serialize};
+use skytable::ConnectionBuilder;
+use skytable::actions::Actions;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -17,9 +22,9 @@ async fn main() {
 		.route("/", get(root))
 		// `POST /users` goes to `create_user`
 		.route("/users", post(create_user))
+		.route("/users", get(get_users))
 		// Add the ExtensionLayer for Skytable
 		.layer(AddExtensionLayer::new(sky));
-
 
 	// run our app with hyper
 	// `axum::Server` is a re-export of `hyper::Server`
@@ -42,7 +47,6 @@ async fn create_user(
 	// as JSON into a `CreateUser` type
 	Json(payload): Json<CreateUser>,
 ) -> impl IntoResponse {
-
 	// insert your application logic here
 	let user = User {
 		id: Uuid::new_v4(),
@@ -55,17 +59,36 @@ async fn create_user(
 	// save user to Skytable
 	con.set(user.id.to_string(), user.username.to_string()).unwrap();
 
-
 	// this will be converted into a JSON response
 	// with a status code of `201 Created`
 	(StatusCode::CREATED, Json(user))
 }
 
-async fn connect_db() -> skytable::ConnectionBuilder {
-	// Handle anything else for setting up the Skytable connection here
-	ConnectionBuilder::new().set_host("127.0.0.1".to_string()).set_port(2003)
+async fn get_users(
+	Extension(sky): Extension<ConnectionBuilder>
+) -> impl IntoResponse {
+	let mut con = sky.get_connection().unwrap();
+	let mut users: Vec<UserResponse> = Vec::new();
+
+	let ids: Vec<String> = con.lskeys(10).unwrap();
+
+	for id in ids {
+		let username = con.get(id.to_string()).unwrap();
+		let u = UserResponse {
+			id,
+			username,
+		};
+		users.push(u)
+	}
+	(StatusCode::OK, Json(users))
 }
 
+async fn connect_db() -> skytable::ConnectionBuilder {
+	// Handle anything else for setting up the Skytable connection here
+	ConnectionBuilder::new()
+		.set_host("127.0.0.1".to_string())
+		.set_port(2003)
+}
 
 // the input to our `create_user` handler
 #[derive(Deserialize)]
@@ -77,5 +100,11 @@ struct CreateUser {
 #[derive(Serialize)]
 struct User {
 	id: uuid::Uuid,
+	username: String,
+}
+
+#[derive(Serialize)]
+struct UserResponse {
+	id: String,
 	username: String,
 }
